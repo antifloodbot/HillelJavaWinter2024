e8edf8201ca37e2d6861019c44e3f7166b4d2dc0
Kostiantyn Talamaniuk <ktalamaniuk@playtika.com>
2024-02-04 23:16:38 +0200

    15 lecture

--- MavenProjects/homework18/src/test/java/hw18/ProductTest.java
+++ MavenProjects/homework18/src/test/java/hw18/ProductTest.java
@@ -11,7 +11,6 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
