509efdc322212bba0ca052a3e4a03e926010cb6b
antifloodbot <36970838+antifloodbot@users.noreply.github.com>
2024-02-05 12:32:09 +0200

    Homework18 (#21)
    homework 18

---
+++ MavenProjects/homework18/src/main/java/hw18/Category.java
@@ -0,0 +1,6 @@
+package hw18;
+
+public enum Category {
+    BOOK, PEN, BAG, PENCIL
+}
+
---
+++ MavenProjects/homework18/src/main/java/hw18/exceptions/CollectionContainsNullException.java
@@ -0,0 +1,7 @@
+package hw18.exceptions;
+
+public class CollectionContainsNullException extends RuntimeException {
+    public CollectionContainsNullException(String message) {
+        super(message);
+    }
+}
